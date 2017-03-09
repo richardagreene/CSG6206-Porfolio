45b5359a0aed2ac15ac2ed80cf29ccf8a5ffb420
Richard <richard.a.greene@gmail.com>
2017-03-09 20:34:45 +0800

    updates of environment variabled

--- Workshop2/network.sh
+++ Workshop2/network.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 clear
-if ! ping -q -c 1 proxy.ecu.edu.au &> /dev/null 
+# Test the network.
+if ping -c 1 proxy.ecu.edu.au &> /dev/null 
 then
-    echo "You are not connected to the ECU Network"
-    exit
+    current_network="ECU";
+else
+    current_network="External";
 fi
 current_date="$(date +'%d/%m/%Y')";
 current_time="$(date +%H:%M:%S)";
-current_system=$(system_profiler SPSoftwareDataType | grep "Computer Name:" | awk -F'(:)+' '{ printf "%s", $2 }') 
 echo "------------------------ System Report ---------------------------"
-echo "Date: $current_date	Time: $current_time 	System Name: $current_system"
+echo "Date: $current_date	Time: $current_time 	System Name: $HOSTNAME"
 echo "------------------------------------------------------------------"
 uptime | awk -F'( |,|:)+' '{ printf "Uptime: %s days %s hours %s minutes\n", $3, $4, $5 }'
 uptime | awk -F'( |,|:)+' '{ printf "Current Users: %s \n", $6 }'
 uptime | awk -F'( |,|:)+' '{ printf "CPU Load: %s \n", $10 }'
-echo "Current Network: $current_system"
+echo "Current Network: $current_network"
 echo "-------------------------- End Report ----------------------------"
