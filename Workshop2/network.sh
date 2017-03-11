bebe5fb7f099763ccf34e57b3832072aa8c2ff42
Richard <richard.a.greene@gmail.com>
2017-03-11 22:00:41 +0800

    minor changes

--- Workshop2/network.sh
+++ Workshop2/network.sh
@@ -9,11 +9,12 @@ else
 fi
 current_date="$(date +'%d/%m/%Y')";
 current_time="$(date +%H:%M:%S)";
+cpu_time="$(uptime | tail -c 6)"; 
 echo "------------------------ System Report ---------------------------"
 echo "Date: $current_date	Time: $current_time 	System Name: $HOSTNAME"
 echo "------------------------------------------------------------------"
-uptime | awk -F'( |,|:)+' '{ printf "Uptime: %s days %s hours %s minutes\n", $3, $4, $5 }'
-uptime | awk -F'( |,|:)+' '{ printf "Current Users: %s \n", $6 }'
-uptime | awk -F'( |,|:)+' '{ printf "CPU Load: %s \n", $10 }'
+uptime | awk -F'(up|,)+' '{ printf "Uptime: %s \n", $2}'
+uptime | awk -F'(,)+' '{ printf "Current Users: %s \n", $2 }'
+echo "CPU Load: $cpu_time"
 echo "Current Network: $current_network"
 echo "-------------------------- End Report ----------------------------"
