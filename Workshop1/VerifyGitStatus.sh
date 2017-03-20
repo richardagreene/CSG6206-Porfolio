3063154b39b10a23420a079ac8f9f2b8437e8810
Richard Greene <rgreene0@our.ecu.edu.au>
2017-03-20 21:12:39 +0800

    error checking and setting format of output

--- Workshop1/VerifyGitStatus.sh
+++ Workshop1/VerifyGitStatus.sh
@@ -1,17 +1,23 @@
 
 #!/bin/bash
 clear
-if [ -z "$1" ]
+if [ -z "$1" ]  # there is no parameter
 then
     echo "Git commit details for $PWD"
     git remote show origin -n | grep "Fetch URL"
     git log --pretty=oneline
+    count=$(git rev-list --all --count -- $1)
+    printf "Total Commits: $count \n"
     exit 0
 fi
-# show file details
-git log --follow $1
-if [ $? -ne 0 ]; then
-    echo "There was an error getting GIT data"
+# show file details Author and Comment
+echo "Checking Git commit details for $1"
+if [ ! -f "$1" ]  # file check
+then
+    echo "Error: File $1 does not exists"
     exit 1
 fi
+git log -1 --format='Author: %aN - %s' --follow $1
+count=$(git rev-list --all --count -- $1)
+printf "Total Commits: $count \n"
 exit 0 # all good so exit
