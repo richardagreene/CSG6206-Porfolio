40ca29b08d44c9a5e82cae42b331816a6792b866
Richard Greene <rgreene0@our.ecu.edu.au>
2017-03-30 23:07:27 +0800

    Added better error checking

--- Workshop4/workshop4.sh
+++ Workshop4/workshop4.sh
@@ -4,21 +4,26 @@
 def binary?(value)
   # regular expression to check value
   # ref: http://stackoverflow.com/questions/14551256/ruby-how-to-find-out-if-a-character-is-a-letter-or-a-digit
-  value =~ /[0-1]/
+  value =~ /[^0-1]/
 end
 
 input = gets.chomp
+if not (input =~ /[^0-1]/).nil? then
+    puts "Error: Only 0-1 values allowed. e.g. 0100001111"
+    exit
+end
+
 prevChar = "0" 
 result = ""
-input.split("").each do |currentChar|
-  # Check the current value is as expected
-  if not binary?(currentChar)
-      raise "Only 0-1 values allowed"
+begin 
+  input.split("").each do |currentChar|
+    # XOR each char against the previous
+    edge = (prevChar.to_i(2) ^ currentChar.to_i(2)).to_s(2)
+    result = result+"#{edge}"
+    prevChar = currentChar
   end
-  # XOR each char against the previous
-  edge = (prevChar.to_i(2) ^ currentChar.to_i(2)).to_s(2)
-  result = result+"#{edge}"
-  prevChar = currentChar
-end
-puts "Input : #{input}"
-puts "Output: #{result}"
+  puts "Input : #{input}"
+  puts "Output: #{result}"
+rescue Exception => e
+  puts 'There was an unexpected error processing. ' + e.message
+end    
