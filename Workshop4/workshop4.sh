b56b08701905d503d0eaac2b001f9fa924441d11
Richard Greene <rgreene0@our.ecu.edu.au>
2017-03-31 11:50:14 +0800

    removed unneeded function

--- Workshop4/workshop4.sh
+++ Workshop4/workshop4.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env ruby
 # Workshop 4 : edge detection
-
-def binary?(value)
-  # regular expression to check value
-  # ref: http://stackoverflow.com/questions/14551256/ruby-how-to-find-out-if-a-character-is-a-letter-or-a-digit
-  value =~ /[^0-1]/
-end
-
+# usage: echo "000011101110011111" | ./workshop4.sh
 input = gets.chomp
 if not (input =~ /[^0-1]/).nil? then
     puts "Error: Only 0-1 values allowed. e.g. 0100001111"
@@ -26,4 +20,4 @@ begin
   puts "Output: #{result}"
 rescue Exception => e
   puts 'There was an unexpected error processing. ' + e.message
-end    
+end
