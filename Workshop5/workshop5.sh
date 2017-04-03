1c4994b1c5dcab5d73b6d4b2e692620eb7713ff8
Richard Greene <rgreene0@our.ecu.edu.au>
2017-04-03 08:59:40 +0800

    added error checking and input validation

--- Workshop5/workshop5.sh
+++ Workshop5/workshop5.sh
@@ -1,54 +1,47 @@
 #!/usr/bin/env ruby
-# Workshop 4 : edge detection
-# Usage: cat input.txt
-#     000011101110011111 
-#     000010101110011111 
-#     000011101110011011 
-#     000010101110011111 
-# cat input.txt | ./workshop4.sh
-
-def GetMaxRowLength(arr)
-  maxRowLength = 0
-  arr.each { |line| 
-    if line.length > maxRowLength then
-      maxRowLength = line.length
-    end
-  }
-  return maxRowLength
-end
+# Workshop 5 : edge detection
+# Usage: 
+#     cat <filename> | ./workshop5.sh
 
 def CalculateEdge(arr)
-  topChar = "0" 
-  result = ""
-  # loop over each line of the array
+  result = ""   
+  # loop over each line of the array of text items
   for lineIndex in 0 ... arr.size
-     prevChar = "0" 
-     for charIndex in 0 ... arr[lineIndex].size
-       currentChar = arr[lineIndex][charIndex]
-        if lineIndex == 0 then 
+     topChar = "0" # start empty
+     prevChar = "0" # start empty 
+     for charIndex in 0 ... arr[lineIndex].chomp.size
+       # consider <whitespace> as '0'
+       currentChar = arr[lineIndex][charIndex].gsub(/\s/,'0')
+       # validation checks 
+        if lineIndex == 0 then
           topChar = currentChar  # assume current char if first row
-        elsif lineIndex > 0 && charIndex > arr[lineIndex-1].size
+        elsif lineIndex > 0 && charIndex > arr[lineIndex-1].chomp.size
           topChar = "0"  # assume "0" if missing on row above
         else
-          topChar = arr[lineIndex-1][charIndex]
+          topChar = arr[lineIndex-1][charIndex].nil? ? "0" : arr[lineIndex-1][charIndex].gsub(/\s/,'0')
         end
-        # XOR char against the previous
+        # XOR char against the previous char to the left of current
         edgeLeft = prevChar.to_i(2) ^ currentChar.to_i(2)
-        # XOR char against the top
+        # XOR char against the char above the current
         edgeTop = currentChar.to_i(2) ^ topChar.to_i(2)
-        # if the top row has changed use the second
+        # if the left char has not changed use the top row
         edge = edgeLeft == 1 ? edgeLeft : edgeTop
         prevChar = currentChar
         result = result+"#{edge}"
      end
-   result = result+"\n"
+   result = "#{result}\n\t"
    end
    return result
 end
 
 # Please each line into an array
 input = $stdin.read
+if not (input =~ /[^0-1\W]/).nil? then
+    puts "Error: Only 0-1 values allowed. e.g. 0100001111"
+    puts "       Whitespace will be considered '0'"
+    exit
+end
 arr = input.split(/\n/)
 result = CalculateEdge(arr)
-puts "Input :   \n#{input}"
-puts "Output:   \n#{result}"
+puts "Input :\t#{input}"
+puts "Output:\t#{result}"
