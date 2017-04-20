27a362f63155202c4b1b975f62886986265e5ac5
Richard Greene <rgreene0@our.ecu.edu.au>
2017-04-20 23:29:36 +1000

    minor change to allow error checking

--- Workshop6/workshop6.js
+++ Workshop6/workshop6.js
@@ -1,6 +1,14 @@
 // Note: required Sharp Library
 //     sudo npm install sharp
-sharp = require('sharp');
+var sharp = require('sharp');
+// check that command was correctly entered
+if(!process.argv[2] || !process.argv[3])
+{
+  console.log("Error: parameters where not passed correctly.");
+  console.log(" syntax: node workshop6 <input filename> <output filename>");
+  return;
+}
+
 console.log("Setting image threshold of " +process.argv[2] + ", output " +process.argv[3])
 sharp(process.argv[2])
   .threshold(200)
