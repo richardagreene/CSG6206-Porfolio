73f3c746281fe3e5e4238e0a1e80cc17a59450fb
Richard Greene <rgreene0@our.ecu.edu.au>
2017-05-30 20:57:57 +0800

    completed error checking

--- Workshop11/workshop11.pl
+++ Workshop11/workshop11.pl
@@ -2,13 +2,23 @@
 use strict;
 use warnings;
 
+die "Error; incorrect parameters, syntax: $0 <filename>\n example: $0 sample.csv\n " if @ARGV != 1;
+die "Error; file should be comma separated value (.csv), syntax: $0 <filename>\n example: $0 sample.csv\n " if index(lc $ARGV[0], ".csv") == -1;
+
+my $index;
 foreach my $line (<>) {
     chomp( $line );
     my @fields = split "," , $line;
-    my $name = @fields[0];
-    my $studentId = @fields[1];
-    my $email = @fields[2];
-    my @courses = split " " , @fields[3];
+    if(scalar @fields < 4 )
+    {
+        print("Line:" . $index . " not formatted correctly\n");
+        $index++;
+        next
+    }
+    my $name = $fields[0];
+    my $studentId = $fields[1];
+    my $email = $fields[2];
+    my @courses = split " " , $fields[3];
     my $studentType = "Undergraduate";
     my $enrolment = "Part Time";
 
@@ -22,7 +32,6 @@ foreach my $line (<>) {
     if($studentType eq "Undergraduate" && scalar @courses > 2 ) {
         $enrolment = "Full time";
     }
-
     if($studentType eq "Postgraduate" && scalar @courses > 1 ) {
         $enrolment = "Full time";
     }
@@ -30,10 +39,10 @@ foreach my $line (<>) {
     print("Name: ". $name . "\n");
     print("Student: " . $studentId . "\n");
     print("Email: " . $email . "\n");
-    print("Units Enrolled: " . @fields[3] . "\n");
+    print("Units Enrolled: " . $fields[3] . "\n");
     print("Enrolment Mode: " . $enrolment . "\n");
     print("Student Type: " . $studentType . "\n\n");
-
+    $index++;
 }
 
 
